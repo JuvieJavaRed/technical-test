1c476766f91a6cddf572951036ac78d5b40683ef
3rdWorldDeveloper <mtho.nyoni@qubedlab.com>
2020-07-12 22:57:36 +0200

    final commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
+import { VirtualScrollerModule } from 'ngx-virtual-scroller';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -17,6 +18,7 @@ import { TabledisplayComponent } from './components/tabledisplay/tabledisplay.co
     AppRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule,
+    VirtualScrollerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/tabledisplay/tabledisplay.component.ts
+++ src/app/components/tabledisplay/tabledisplay.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {TabledisplayService} from '../../services/tabledisplay.service';
+import { IPageInfo } from 'ngx-virtual-scroller';
+
+
 
 @Component({
   selector: 'app-tabledisplay',
@@ -7,16 +10,68 @@ import {TabledisplayService} from '../../services/tabledisplay.service';
   styleUrls: ['./tabledisplay.component.css']
 })
 export class TabledisplayComponent implements OnInit {
-
+  //data variable to get back the request from the api
   tableData:any;
+
+  //javascript array to hold the  reply from the api with javascript objects
+  tableasObject:any = [];
+
+  //object used to push key value conversion into array of objects. 
+  pushArray:any = {};
 
   constructor(private tabledisplayService: TabledisplayService) { }
 
+  loading: boolean;
+
+
   ngOnInit(): void {
     this.tabledisplayService.getTableData().then(data=>{
-        this.tableData = data;
-        console.log(this.tableData);
+        this.tableData = data; // bring in data from api
+        for(let i=0; i<this.tableData.length; i++){
+              //data comes back without key names in key value pair
+              this.pushArray= {
+                transaction:this.tableData[i][2],
+                amount:this.tableData[i][4],
+                date: this.tableData[i][1],
+                address:this.tableData[i][3],
+              }
+
+              this.tableasObject.push(this.pushArray);
+
+              //upadate state with values to use for cursor........
+
+       }
+       console.log(this.tableasObject);
     })
   }
+
+  //method to update the scroll view
+  fetchMore(event: IPageInfo) {
+    if (event.endIndex !== this.tableasObject.length-1) return;
+    this.loading = true;
+    console.log(this.tableasObject[this.tableasObject.length][0]);
+    //when at end of the situation load newer or older data
+    this.tabledisplayService.updateTableData(this.tableasObject[this.tableasObject.length][0]).then(data => {
+      this.tableData = data; // bring in data from api
+      for(let i=0; i<this.tableData.length; i++){
+        //data comes back without key names in key value pair
+        this.pushArray= {
+          transaction:this.tableData[i][2],
+          amount:this.tableData[i][4],
+          date: this.tableData[i][1],
+          address:this.tableData[i][3],
+        }
+
+        this.tableasObject.push(this.pushArray);
+
+      }
+      this.loading = false;
+  }, () => this.loading = false);
+  
+  }
+
+  
+ 
+
 
 }
